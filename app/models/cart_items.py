168a47b35dc03623b99f1c4362a28cc36710b57c
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-18 02:11:26 +0500

    Доработки модели позиции в корзине

--- app/models/cart_items.py
+++ app/models/cart_items.py
@@ -1,39 +1,39 @@
-from typing import TYPE_CHECKING
-from app.database import Base
-from sqlalchemy import UniqueConstraint, ForeignKey, DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import DateTime, ForeignKey, UniqueConstraint, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.database import Base
 
 if TYPE_CHECKING:
-    from .users import User
-    from .products import Product
+    from .products import ProductORM
+    from .users import UserORM
 
 
-class CartItem(Base):
-    __tablename__ = 'cart_items'
-
+class CartItemORM(Base):
+    __tablename__ = "cart_items"
     __table_args__ = (
-        UniqueConstraint('user_id', 'product_id', name='uq_cart_items_user_product'),
+        UniqueConstraint("user_id", "product_id", name="uq_cart_items_user_product"),
     )
 
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(
-        ForeignKey('users.id', ondelete='CASCADE'),
-        nullable=False,
-        index=True
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True
     )
     product_id: Mapped[int] = mapped_column(
-        ForeignKey('products.id', ondelete='CASCADE'),
-        nullable=False,
-        index=True
+        ForeignKey("products.id", ondelete="CASCADE"), nullable=False, index=True
     )
-    quantity: Mapped[int] = mapped_column(nullable=False, default=1)
+    quantity: Mapped[int]
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False, onupdate=func.now()
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+        onupdate=func.now(),
     )
 
-    user: Mapped['User'] = relationship('User', back_populates='cart_items')
-    product: Mapped['Product'] = relationship('Product', back_populates='cart_items')
+    user: Mapped["UserORM"] = relationship(back_populates="cart_items")
+    product: Mapped["ProductORM"] = relationship(back_populates="cart_items")
