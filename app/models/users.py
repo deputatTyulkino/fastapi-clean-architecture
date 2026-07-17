05330a4dbf4271c13f3bace0850523f4cd721280
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-17 19:19:55 +0500

    Доработки модели пользователей

--- app/models/users.py
+++ app/models/users.py
@@ -1,33 +1,31 @@
 from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, Boolean
 
-from app.database import Base
+from app.core.database import Base
+from app.domain.models.users import UserRole
 
 if TYPE_CHECKING:
-    from .products import Product
-    from .reviews import Review
     from .cart_items import CartItem
     from .orders import Order
+    from .products import Product
+    from .reviews import Review
 
 
-class User(Base):
-    __tablename__ = 'users'
+class UserORM(Base):
+    __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    role: Mapped[str] = mapped_column(String, default='buyer')
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool]
+    role: Mapped[UserRole]
 
-    products: Mapped[list['Product']] = relationship(
-        'Product', back_populates='seller'
+    products: Mapped[list["Product"]] = relationship(back_populates="seller")
+    reviews: Mapped[list["Review"]] = relationship(back_populates="user")
+    cart_items: Mapped[list["CartItem"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
     )
-    reviews: Mapped['Review'] = relationship('Review', back_populates='user')
-    cart_items: Mapped['CartItem'] = relationship(
-        'CartItem', back_populates='user', cascade='all, delete-orphan'
-    )
-    orders: Mapped[list['Order']] = relationship(
-        'Order', back_populates='user', cascade='all, delete-orphan'
+    orders: Mapped[list["Order"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
     )
