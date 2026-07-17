c13516d0677bb0dbc414ba5543d9826b5a8542b5
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-17 20:43:36 +0500

    Доработки модели категорий

--- app/models/categories.py
+++ app/models/categories.py
@@ -6,13 +6,13 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.core.database import Base
 
 if TYPE_CHECKING:
-    from .products import Product
+    from .products import ProductORM
 
 
-class CategoryOrm(Base):
+class CategoryORM(Base):
     __tablename__ = "categories"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool]
-    products: Mapped[list["Product"]] = relationship(back_populates="category")
+    products: Mapped[list["ProductORM"]] = relationship(back_populates="category")
