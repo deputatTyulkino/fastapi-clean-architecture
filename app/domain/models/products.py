6113c022ce676f39740ca61226a11f7425c45015
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-17 20:42:49 +0500

    Добавление датакласса для продуктов

---
+++ app/domain/models/products.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+from decimal import Decimal
+
+
+@dataclass
+class ProductDomain:
+    name: str
+    price: Decimal
+    stock: int
+    category_id: int
+    seller_id: int
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    id: int | None = None
+    description: str | None = None
+    image_url: str | None = None
+    is_active: bool = True
+    rating: Decimal = Decimal(0.0)
