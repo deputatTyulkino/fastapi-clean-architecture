ac33f0c0a4390707163ceaefee50448f61c7179a
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-19 01:13:42 +0500

    Доработки

--- app/infrastructure/models/__init__.py
+++ app/infrastructure/models/__init__.py
@@ -1,8 +1,16 @@
-from .categories import Category
-from .products import Product
-from .users import User
-from .reviews import Review
-from .cart_items import CartItem
-from .orders import Order, OrderItem
+from .cart_items import CartItemORM
+from .categories import CategoryORM
+from .orders import OrderItemORM, OrderORM
+from .products import ProductORM
+from .reviews import ReviewORM
+from .users import UserORM
 
-__all__ = ["Category", "Product", "User", "Review", "CartItem", "Order", "OrderItem"]
+__all__ = [
+    "CategoryORM",
+    "ProductORM",
+    "UserORM",
+    "ReviewORM",
+    "CartItemORM",
+    "OrderORM",
+    "OrderItemORM",
+]
