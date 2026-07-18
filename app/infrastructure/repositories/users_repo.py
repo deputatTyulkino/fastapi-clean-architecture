e341b5ac36983c9480c70510fe4433074385ff2d
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-19 01:49:22 +0500

    Реализация класса репо для пользователей

---
+++ app/infrastructure/repositories/users_repo.py
@@ -0,0 +1,42 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.domain.interfaces_repo.i_user_repo import IUserRepo
+from app.domain.models.users import UserDomain
+from app.infrastructure.models.users import UserORM
+
+
+class UserRepo(IUserRepo):
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    def _to_orm_model(self, user: UserDomain) -> UserORM:
+        return UserORM(
+            id=user.id,
+            email=user.email,
+            hashed_password=user.hashed_password,
+            is_active=user.is_active,
+            role=user.role,
+        )
+
+    def _to_domain_model(self, user: UserORM) -> UserDomain:
+        return UserDomain(
+            id=user.id,
+            email=user.email,
+            hashed_password=user.hashed_password,
+            is_active=user.is_active,
+            role=user.role,
+        )
+
+    async def get_by_email(self, email: str) -> UserDomain | None:
+        query = select(UserORM).filter_by(email=email)
+        user = (await self.db.execute(query)).scalar_one_or_none()
+        if user is None:
+            return None
+        return self._to_domain_model(user)
+
+    async def create(self, user: UserDomain) -> UserDomain:
+        new_user = self._to_orm_model(user)
+        self.db.add(new_user)
+        await self.db.flush()
+        return self._to_domain_model(new_user)
