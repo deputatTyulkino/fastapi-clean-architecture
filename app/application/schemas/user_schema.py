aef38aef2ebff370577175abf276ba9aa0be37cb
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-19 01:42:49 +0500

    Добавление схем для поьзователей

---
+++ app/application/schemas/user_schema.py
@@ -0,0 +1,55 @@
+import re
+from typing import Self
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    EmailStr,
+    Field,
+    field_validator,
+    model_validator,
+)
+
+
+class UserSchema(BaseModel):
+    id: int
+    email: EmailStr
+    role: str
+    is_active: bool
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserResponseSchema(BaseModel):
+    user: UserSchema
+    access: str
+    refresh: str
+
+
+class LoginUserSchema(BaseModel):
+    email: EmailStr = Field(..., description="Email пользователя")
+    password: str = Field(..., description="Пароль", min_length=8)
+
+
+class RegisterUserSchema(LoginUserSchema):
+    password_confirm: str
+
+    @field_validator("password")
+    @classmethod
+    def password_validator(cls, value):
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Пароль должен содержать заглавные буквы")
+        if not re.search(r"\d", value):
+            raise ValueError("Пароль должен содержать цифры")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Пароль должен содержать строчные буквы")
+        return value
+
+    @model_validator(mode="after")
+    def check_equal_passwords(self) -> Self:
+        if self.password != self.password_confirm:
+            raise ValueError("Пароли не совпадают")
+        return self
+
+
+class RefreshTokenSchema(BaseModel):
+    refresh: str
