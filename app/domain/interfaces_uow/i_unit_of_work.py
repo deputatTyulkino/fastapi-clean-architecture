c92ac7cbe9d9db06a5626dc82e0dc6c4f3a382bf
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-20 03:49:27 +0500

    Добавление метода для работы с сущностью пользователей и добавление параметров в метод __aexit__

--- app/domain/interfaces_uow/i_unit_of_work.py
+++ app/domain/interfaces_uow/i_unit_of_work.py
@@ -4,12 +4,17 @@ from typing import Self
 from app.domain.interfaces_repo.i_user_repo import IUserRepo
 
 
-
 class IUnitOfWork(ABC):
-    users: IUserRepo
+    @property
+    @abstractmethod
+    def users(self) -> IUserRepo:
+        raise NotImplementedError()
+
+    @abstractmethod
     async def __aenter__(self) -> Self:
         raise NotImplementedError()
 
+    @abstractmethod
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         raise NotImplementedError()
 
