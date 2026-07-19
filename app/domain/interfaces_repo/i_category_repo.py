aa359804f9544f58f097f6d1e77c153aa927a2ad
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-20 04:09:42 +0500

    Первая версия интерфейса репо категорий

---
+++ app/domain/interfaces_repo/i_category_repo.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+from app.domain.models.categories import CategoryDomain
+
+
+class ICategoryRepo(ABC):
+    @abstractmethod
+    async def get_all(self) -> list[CategoryDomain]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_by_id(self, id: int) -> CategoryDomain:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def get_by_name(self, name: str) -> CategoryDomain:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def create(self, category_data: CategoryDomain) -> CategoryDomain:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def update(self, id: int, category_data: CategoryDomain) -> CategoryDomain:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def delete(self, id: int) -> int:
+        raise NotImplementedError()
