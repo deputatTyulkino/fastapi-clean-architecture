6d3a04af8bac92107fb0d5bdfbd31b2d20b1dcd8
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-20 03:47:45 +0500

    Перенос зависимости по получению сессии

--- app/core/depends.py
+++ app/core/depends.py
@@ -1,18 +1,7 @@
-from collections.abc import AsyncGenerator
-
-from fastapi import Request
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.core.auth import Security
 from app.core.config import get_settings
 
 
-async def get_db(request: Request) -> AsyncGenerator[AsyncSession, None]:
-    session_factory = request.app.state.session_factory
-    async with session_factory() as session:
-        yield session
-
-
-async def get_security() -> Security:
+def get_security() -> Security:
     settings = get_settings()
     return Security(settings)
