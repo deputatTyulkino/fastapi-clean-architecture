0a28d0d3ba7c1eeae13bed6df88c1fa02e4ebea5
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-25 05:33:13 +0500

    Разделение логики

---
+++ app/presentation/depends/entities/users_services.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from app.application.services.users_services import UserServices
+from app.infrastructure.depends.users_services import get_users_services_infr
+
+
+def get_users_servcies(
+    services: Annotated[UserServices, Depends(get_users_services_infr)],
+):
+    return services
