bf0842f443be587d787efe90cf34210d2bb94b92
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-25 05:38:34 +0500

    Подключение ручек продуктов

--- app/core/main.py
+++ app/core/main.py
@@ -3,12 +3,14 @@ from fastapi.staticfiles import StaticFiles
 
 from app.core.lifespan import lifespan
 from app.presentation.routers.categories import router as categories_router
+from app.presentation.routers.products import router as products_router
 from app.presentation.routers.users import router as users_router
 
 app = FastAPI(title="FastAPI Интернет-магазин", lifespan=lifespan)
 
 app.include_router(users_router)
 app.include_router(categories_router)
+app.include_router(products_router)
 
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
