a29e679b1790c1d58a083ae380ad149c68566622
Tigran Petrosyan <tigr-080807@mail.ru>
2026-07-25 05:31:27 +0500

    Добавлена функция для получения класса сервисов

---
+++ app/presentation/depends/entities/products_services.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from fastapi import Depends
+
+from app.application.services.products_services import ProductServices
+from app.infrastructure.depends.products_services import get_products_services_infr
+
+
+def get_product_services(
+    services: Annotated[ProductServices, Depends(get_products_services_infr)],
+) -> ProductServices:
+    return services
